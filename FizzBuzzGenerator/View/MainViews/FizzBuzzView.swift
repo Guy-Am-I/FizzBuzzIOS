05d5f55ee3558d2ffd8fcaaf4a1cace71d0675e8
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-16 16:35:48 -0700

    Added simple fizz buzz main view

--- FizzBuzzGenerator/View/MainViews/FizzBuzzView.swift
+++ FizzBuzzGenerator/View/MainViews/FizzBuzzView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct FizzBuzzView: View {
+    let MAX = 100
     var body: some View {
         ZStack {
             Color
@@ -15,8 +16,32 @@ struct FizzBuzzView: View {
                 .opacity(0.4)
                 .ignoresSafeArea()
             
-            Text("FizzBuzz")
+            ScrollView {
+                LazyVStack(spacing: 10) {
+                    ForEach(0..<MAX, id: \.self) { i in
+                        if i % 15 == 0 {
+                            FormattedText(text: "FizzBuzz")
+                        } else if i % 3 == 0  {
+                            FormattedText(text: "Fizz")
+                        } else if i % 5 == 0 {
+                            FormattedText(text: "Buzz")
+                        } else {
+                            FormattedText(text: "\(i)")
+                        }
+                    }
+                }
+            }
         }
+    }
+}
+
+struct FormattedText : View {
+    let text: String
+    var body: some View {
+        Text(text)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .foregroundStyle(Color("AccentColor"))
     }
 }
 
