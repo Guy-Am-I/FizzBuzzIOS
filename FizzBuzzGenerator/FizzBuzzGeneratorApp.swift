5f93c8fdcbf3689e218bae041aeb311ba5473826
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-16 11:52:42 -0700

    Initial Commit

---
+++ FizzBuzzGenerator/FizzBuzzGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzGeneratorApp.swift
+//  FizzBuzzGenerator
+//
+//  Created by Guy Steinberg on 2024-04-16.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
