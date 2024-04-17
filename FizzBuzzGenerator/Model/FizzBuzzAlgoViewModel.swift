b63f0ca150e6b9a60a6d1e3ae2142baee4f3aee9
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-16 17:27:02 -0700

    added implementation initializers for python and cpp

--- FizzBuzzGenerator/Model/FizzBuzzAlgoViewModel.swift
+++ FizzBuzzGenerator/Model/FizzBuzzAlgoViewModel.swift
@@ -61,7 +61,7 @@ class FizzBuzzAlgoViewModel: ObservableObject {
                   loopFunc: .oneArgFunc{ code in
                   """
                     for(int i = 1; i <= MAX; ++i) {
-                        \(code)
+                        \(code.indent())
                     }
                   """},
                   ifFunc: .twoArgFunc { logic, code in
@@ -90,7 +90,83 @@ class FizzBuzzAlgoViewModel: ObservableObject {
                   },
                   printIdxFunc: .zeroArgFunc {
                       #"printf("%d\t", i);"#
-                  })
+                  }),
+            .init(name: "C++",
+                  versionNumber: 1.0,
+                  imports: "#include <iostream>",
+                  maxConstDef: "const int MAX = 100;",
+                  boilerplate: .oneArgFunc { code in
+                  """
+                  int main() {
+                    \(code)
+                  }
+                  """
+                  },
+                  loopFunc: .oneArgFunc{ code in
+                  """
+                    for(int i = 1; i <= MAX; ++i) {
+                        \(code.indent())
+                    }
+                  """},
+                  ifFunc: .twoArgFunc { logic, code in
+                  """
+                  if (\(logic)) {
+                    \(code)
+                  }
+                  """
+                  },
+                  elseIfFunc: .twoArgFunc { logic, code in
+                  """
+                  else if (\(logic)) {
+                    \(code)
+                  }
+                  """
+                  },
+                  elseFunc: .oneArgFunc { code in
+                  """
+                  else {
+                      \(code)
+                  }
+                  """
+                  },
+                  printFunc: .oneArgFunc { str in
+                      #"std::cout << \#(str)\t << std::endl;"#
+                  },
+                  printIdxFunc: .zeroArgFunc {
+                      #"std::cout << i\t << std::endl;"#
+                  }),
+            .init(name: "Python",
+                  maxConstDef: "MAX = 100",
+                  commentCode: { code in return #"# "# + code},
+                  loopFunc: .oneArgFunc{ code in
+                  """
+                    for i in range(1, MAX):
+                        \(code)
+                  """},
+                  ifFunc: .twoArgFunc { logic, code in
+                  """
+                  if \(logic):
+                    \(code)
+                  """
+                  },
+                  elseIfFunc: .twoArgFunc { logic, code in
+                  """
+                  elif \(logic):
+                    \(code)
+                  """
+                  },
+                  elseFunc: .oneArgFunc { code in
+                  """
+                  else:
+                      \(code)
+                  """
+                  },
+                  printFunc: .oneArgFunc { str in
+                      #"print("\#(str)\t", end = '')"#
+                  },
+                  printIdxFunc: .zeroArgFunc {
+                      #"print(f"{i}\t", end='')"#
+                  }),
         ]
     }
 }
